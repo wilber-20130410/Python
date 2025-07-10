841e0626cb8f0ab11c47b4effc743d8ff07df497
wilber <xuwb0410@163.com>
2025-07-10 20:56:50 +0800

    Update U1_nines.py

--- U1_nines.py
+++ U1_nines.py
@@ -1,4 +1,4 @@
-for y in range(1,10):
-    for x in range(1,y+1):
-        print('%dX%d = %d'% (x,y,x*y), end=' ')
+for y in range(1, 10):
+    for x in range(1, y + 1):
+        print('%dX%d = %d'% (x, y, x  *y), end = ' ')
     print('')
